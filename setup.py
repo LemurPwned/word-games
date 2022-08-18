0a7193dc75278f1c73b078ac4c57cd91255cae3f
LemurPwned <jakub.mojsiejuk@gmail.com>
2022-08-18 23:24:55 +0200

    missing inits and setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="word_games",
+        version="0.1.0",
+        author="Alexander Orlovski",
+        author_email="orlov@def.inr",
+        description="Word games",
+        long_description=open("README.md").read(), 
+    )
